6d5bc7fe382c8502ed8cb79466dc4934c829197e
7-Dodi <douglassilvaaraujo78@gmail.com>
2025-02-12 14:36:08 -0300

    feat: Creating page for vaccines

---
+++ src/pages/Vaccines-Page/index.tsx
@@ -0,0 +1,114 @@
+//Importações
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+
+//Estilização
+import "./style.css";
+
+//Componentes
+import { Header } from "../../components/Header-Component";
+import { OptionsDoctorsSlider } from "../../Sliders/OptionsDoctors-slider";
+import { RegisterVaccination } from "../../components/RegisterVaccination-Component";
+import { Footer } from "../../components/Footer-Component";
+
+//Types
+import { VaccineDTO } from "../../types/vaccineTypes";
+
+//Contextos
+import { useVaccine } from "../../Contexts/VaccineContext";
+
+//Class
+export const VaccinesPage = () => {
+    //Definindo o contexto
+    const { allVaccines } = useVaccine();
+
+    //State
+    const [allListVaccines, setAllListVaccines] = useState<VaccineDTO[]>();
+    const [visibleVaccines, setVisibleVaccines] = useState<VaccineDTO[]>();
+    const [filter, setFilter] = useState<string>("");
+
+    //Effect
+    useEffect(() => {
+        (() => {
+            setAllListVaccines(allVaccines);
+            setVisibleVaccines(allVaccines);
+        })();
+    }, [allVaccines]);
+
+    useEffect(() => {
+        (() => {
+            if (filter !== "") {
+                switch (filter) {
+                    case "withEvents":
+                        setVisibleVaccines(allListVaccines?.filter(vaccine => vaccine.vaccinationCalendar.length > 0));
+                        break;
+                    case "recent":
+                        setVisibleVaccines(allListVaccines?.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()));
+                        break;
+                    case "inEvents":
+                        setVisibleVaccines(allListVaccines?.filter(vaccine => vaccine.vaccinationCalendar.length > 0));
+                        break;
+                    default:
+                        setVisibleVaccines(allListVaccines);
+                        break;
+                }
+            }
+        })();
+    }, [filter]);
+
+    //Funções
+    const handleSearch = (query: string) => {
+        if (query === "") {
+            setVisibleVaccines(allListVaccines);
+        } else {
+            setVisibleVaccines(allListVaccines?.filter(vaccine => vaccine.name.toLowerCase().includes(query.toLowerCase())));
+        }
+    };
+
+    return (
+        <div className="container flex apresentation vaccineScreen" id="vaccineScreen">
+            <Header
+                searchVisibility={true}
+                actionPage="Vaccine"
+                titleMenu="Vacinas cadastradas"
+                functionSearch={handleSearch}
+            />
+
+            <div className="apresentationContainer flex">
+
+                <div className="filter-container flex">
+                    <h3 className="filterVaccines">
+                        Listar vacinas por:
+                    </h3>
+                    <select
+                        className="filterSelect"
+                        value={filter}
+                        onChange={(e) => setFilter(e.target.value)}
+                    >
+                        <option value="">Selecione um filtro</option>
+                        <option value="withEvents">Vacinas com eventos</option>
+                        <option value="recent">Vacinas recentes adicionadas</option>
+                        <option value="inEvents">Vacinas em eventos</option>
+                    </select>
+                </div>
+
+
+                <div className="gridListVaccines flex">
+
+                    <div className="listVaccines grid">
+                        {visibleVaccines?.map(vaccine => (
+                            <div className="itemVaccine flex" key={vaccine.id}>
+                                <p className="itemVaccine-name">{vaccine.name}</p>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="calendarVaccines"></div>
+                </div>
+
+                <OptionsDoctorsSlider />
+                <Footer systemPages={true} />
+            </div>
+        </div>
+    )
+}
